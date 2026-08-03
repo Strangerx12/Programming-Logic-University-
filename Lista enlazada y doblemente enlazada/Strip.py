029c50e5c8799b707017235b226e7ff8aa69dccc
Kevin <y448bkhctk@privaterelay.appleid.com>
2026-08-02 20:32:00 -0500

    Update Strip.py

--- Lista enlazada y doblemente enlazada/Strip.py
+++ Lista enlazada y doblemente enlazada/Strip.py
@@ -1,4 +1,5 @@
-correo_con_espacios = "   usuario.prueba@email.com "
-correo_limpio = correo_con_espacios
+#Limpiar correo que ingreso con espacios en blanco al inicio y al final
+correo_con_espacios = "   usuario.prueba@gmail.com   "
+correo_limpio = correo_con_espacios.strip() #Quitar espacios en blanco al inicio y al final (strip)
 
 print("Correo limpio:", correo_limpio)
