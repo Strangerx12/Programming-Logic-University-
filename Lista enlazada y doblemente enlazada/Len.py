084ce6488edac43dd62fc092a8c3f181545754e9
Kevin <y448bkhctk@privaterelay.appleid.com>
2026-08-02 20:31:08 -0500

    Len.py

--- Lista enlazada y doblemente enlazada/Len.py
+++ Lista enlazada y doblemente enlazada/Len.py
@@ -1,4 +1,5 @@
+# Contar el número de elementos en una lista utilizando la función len()
 lista_tareas = ["Lavar ropa", "Hacer ejercicio", "Pagar luz", "Estudiar"]
-total_tareas = len(lista_tareas)
+total_tareas = len(lista_tareas) # len toma la longitud o número de elementos en la lista
 
 print("Número total de tareas:", total_tareas)
