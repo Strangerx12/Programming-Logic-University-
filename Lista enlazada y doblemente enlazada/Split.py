073e4fd3ed96ed3dd70294e41c5eb270dde75dc9
Kevin <y448bkhctk@privaterelay.appleid.com>
2026-08-02 20:31:39 -0500

    Split.py

--- Lista enlazada y doblemente enlazada/Split.py
+++ Lista enlazada y doblemente enlazada/Split.py
@@ -1,4 +1,5 @@
+## Dividir una ruta de directorio en partes utilizando el método split()
 ruta_directorio = "C:/Usuarios/Documentos/Proyectos/Python"
-partes_ruta = ruta_directorio.split("s")
+partes_ruta = ruta_directorio.split("s") # Dividir la ruta por el carácter "s"
 
 print("Partes de la ruta:", partes_ruta)
