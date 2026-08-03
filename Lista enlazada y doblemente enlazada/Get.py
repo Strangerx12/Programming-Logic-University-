b5a5fcec9e970fb615a2e2710ddba6b02f5a2407
Kevin <y448bkhctk@privaterelay.appleid.com>
2026-08-02 20:30:48 -0500

    Get.py

--- Lista enlazada y doblemente enlazada/Get.py
+++ Lista enlazada y doblemente enlazada/Get.py
@@ -1,4 +1,5 @@
+# Obtener el precio de un producto utilizando el método get() en un diccionario
 inventario_tienda = {"manzana": 1.50, "pera": 2.00, "uva": 3.25}
-precio_manzana = inventario_tienda.get("manzana")
+precio_manzana = inventario_tienda.get("manzana") # En caso de que no exista la clave "manzana" se retornará None
 
 print("Precio de la manzana:", precio_manzana)
